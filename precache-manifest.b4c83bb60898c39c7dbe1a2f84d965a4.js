8bf37477343f24d8cc922f7865d897325237b093
GhabiHamza <ghabihamza77@gmail.com>
2019-08-28 18:04:41 +0200

    Updates

---
+++ precache-manifest.b4c83bb60898c39c7dbe1a2f84d965a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5ff4b2ddcb15da13b6a2e3d14f9cc4",
+    "url": "/complex-creditcard/index.html"
+  },
+  {
+    "revision": "936ac720784b353f39ac",
+    "url": "/complex-creditcard/static/css/main.7a075fae.chunk.css"
+  },
+  {
+    "revision": "7fe91e8bc7b2fa5aff34",
+    "url": "/complex-creditcard/static/js/2.d4bd0e10.chunk.js"
+  },
+  {
+    "revision": "936ac720784b353f39ac",
+    "url": "/complex-creditcard/static/js/main.15732971.chunk.js"
+  },
+  {
+    "revision": "56d9fc1e1f1eafa9a949",
+    "url": "/complex-creditcard/static/js/runtime~main.e713fbce.js"
+  },
+  {
+    "revision": "a348e897fd16171c65e24bc816d5a792",
+    "url": "/complex-creditcard/static/media/images.a348e897.jpg"
+  }
+]);
